e1bfc33eedb971ee49166aa7b802f8ac0af10ba0
Yago Carvalho <yago.meira@poli.ufrj.br>
2017-11-26 13:16:33 -0200

    Created solution

---
+++ TrabalhoOtim/TrabalhoOtim/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoOtim
+{
+    class Program
+    {
+        private static double[] _x0;
+
+        static void Main (string[] args)
+        {
+            if (!ParseOptions())
+            {
+                Console.WriteLine ("Error parsing options!");
+                return;
+            }
+
+            for (int i = 0; i < _x0.Length; i++)
+            {
+                Console.WriteLine (_x0[i].ToString());
+            }
+
+            Console.ReadKey ();
+        }
+
+        private static bool ParseOptions ()
+        {
+            string x0 = ConfigurationManager.AppSettings.Get ("x0");
+
+            try
+            {
+                string[] splittedArray = x0.Split (';');
+                _x0 = new double[] { double.Parse(splittedArray[0]), double.Parse (splittedArray[1]) };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine (ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
